4b92eba29d12dc8897b53d4c1631843a9f684fed
E T <ellis.trickett@gmail.com>
2025-10-23 14:16:57 +0100

    Initial Commit Express

---
+++ app.js
@@ -0,0 +1,23 @@
+//app.js
+
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res)=>{
+    res.status(200);
+    res.send("Welcome to root URL of Server");
+});
+app.get('/helloworld', (req, res)=>{
+    res.status(200);
+    res.send("Hello World");
+});
+
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running,and App is listening on port "+ PORT);
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
