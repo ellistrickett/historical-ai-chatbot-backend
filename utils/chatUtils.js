1e46d395aafde3b72b6a1c6d61cb87e79645a67f
E T <ellis.trickett@gmail.com>
2025-12-04 20:25:25 +0000

    bugfix: detectDialogueTree now only matches whole words

--- utils/chatUtils.js
+++ utils/chatUtils.js
@@ -3,7 +3,7 @@ export function chooseWeighted(items) {
 
     const totalWeight = items.reduce((sum, item) => sum + item.probability, 0);
     let random = Math.random() * totalWeight;
-    
+
     for (const item of items) {
         random -= item.probability;
         if (random <= 0) {
@@ -15,24 +15,27 @@ export function chooseWeighted(items) {
 
 export function detectTopic(userText, topicsMap) {
     const lowerInput = userText.toLowerCase();
-    
+
     for (const [topic, keywords] of Object.entries(topicsMap)) {
         if (keywords.some(keyword => lowerInput.includes(keyword.toLowerCase()))) {
             return topic;
         }
     }
-    return null; 
+    return null;
 }
 
 export function detectDialogueTree(userText, dialogueTrees) {
     const lowerInput = userText.toLowerCase();
-    
+
     if (!dialogueTrees) return null;
 
     for (const [treeName, treeData] of Object.entries(dialogueTrees)) {
-        if (treeData.triggers.some(trigger => lowerInput.includes(trigger))) {
-            return { name: treeName, step: "start" }; 
+        if (treeData.triggers.some(trigger => {
+            const regex = new RegExp(`\\b${trigger}\\b`, 'i');
+            return regex.test(lowerInput);
+        })) {
+            return { name: treeName, step: "start" };
         }
     }
-    return null; 
+    return null;
 }
